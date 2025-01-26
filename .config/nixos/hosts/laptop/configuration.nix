44a096d21fee71992fcddb19b9ef871b2dca617d
areif-dev <aj@ajreifsnyder.com>
2025-01-26 00:46:17 -0500

    Nixos: remove password login for root

--- .config/nixos/hosts/laptop/configuration.nix
+++ .config/nixos/hosts/laptop/configuration.nix
@@ -102,6 +102,9 @@
     ];
   };
 
+  # Disable root password login
+  users.users.root.hashedPassword = "!";
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
