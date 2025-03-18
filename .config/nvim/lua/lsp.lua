f3a8fe60c160f214867da270593553f167fa910c
areif-dev <aj@ajreifsnyder.com>
2025-03-18 15:21:02 -0400

    nvim: add gopls to lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -5,14 +5,16 @@ if vim.fn.executable("rust-analyzer") == 1 then
   lspconfig.rust_analyzer.setup({
     capabilities = capabilities
   })
-else
-  print("rust-analyzer not found")
 end
 
 if vim.fn.executable("pyright") == 1 then 
   lspconfig.pyright.setup({
     capabilities = capabilities,
   })
-else 
-  print("pyright not found")
+end
+
+if vim.fn.executable("gopls") == 1 then 
+  lspconfig.gopls.setup({
+    capabilities = capabilities,
+  })
 end
