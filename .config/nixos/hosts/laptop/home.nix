7abd73c46349343dee239bf310aea0741359ff6d
areif-dev <aj@ajreifsnyder.com>
2025-03-27 20:32:23 -0400

    nixos.laptop: update vscode settings

--- .config/nixos/hosts/laptop/home.nix
+++ .config/nixos/hosts/laptop/home.nix
@@ -80,19 +80,32 @@
       package = pkgs.vscodium; 
       profiles.default = {
         extensions = with pkgs.vscode-extensions; [
+          catppuccin.catppuccin-vsc
+          esbenp.prettier-vscode
           golang.go 
           ms-python.python 
           rust-lang.rust-analyzer
           vscodevim.vim
         ];
         userSettings = {
-          "files.associations": {
+          "files.associations" = {
             "*.gohtml" = "html";
           };
+          "[javascript]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+          };
+          "[css]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+          };
+          "[html]" = {
+            "editor.defaultFormatter" = "vscode.html-language-features";
+          };
           "editor.linkedEditing" = true;
+          "editor.formatOnSave" = true;
           "html.autoClosingTags" = true;
           "javascript.autoClosingTags" = true;
           "typescript.autoClosingTags" = true;
+          "workbench.colorTheme" = "Catppuccin Mocha";        
         };
       };
     };
