ebee6086e4abd9d6d578449e9f3527a5852fd28a
areif-dev <aj@ajreifsnyder.com>
2025-10-29 12:13:34 -0400

    scripts: don't suspend desktop (need to be able to ssh into it)

--- .config/scripts/idle.sh
+++ .config/scripts/idle.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash 
 
+suspend_cmd='$HOME/.config/scripts/monitor-power.sh --off && systemctl suspend'
+if [ $(hostname) = "desktop" ]; then 
+    suspend_cmd='$HOME/.config/scripts/monitor-power.sh --off' 
+fi
+
 # Lock the screen after 2 minutes of inactivity. Turn the monitor off and suspend power after 20 minutes
 swayidle -w timeout 120 'swaylock -f -c 000000' \
-    timeout 1200 '$HOME/.config/scripts/monitor-power.sh --off && systemctl suspend' \
+    timeout 1200 $suspend_cmd \
     resume '$HOME/.config/scripts/monitor-power.sh --on && $HOME/.config/scripts/monitor-scale.sh' \
     before-sleep 'swaylock -f -c 000000' 
 
