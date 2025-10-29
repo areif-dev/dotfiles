27a15b568342169f353409d33a8e8d6f85f75af2
areif-dev <aj@ajreifsnyder.com>
2025-10-29 12:46:27 -0400

    scrips: remove all suspend from idle. Need ssh on laptop as well

--- .config/scripts/idle.sh
+++ .config/scripts/idle.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash 
 
-suspend_cmd='$HOME/.config/scripts/monitor-power.sh --off && systemctl suspend'
-if [ $(hostname) = "desktop" ]; then 
-    suspend_cmd='$HOME/.config/scripts/monitor-power.sh --off' 
-fi
-
 # Lock the screen after 2 minutes of inactivity. Turn the monitor off and suspend power after 20 minutes
-swayidle -w timeout 120 'swaylock -f -c 000000' \
-    timeout 1200 $suspend_cmd \
+swayidle -w timeout 120 'swaylock -f -c 000000 && $HOME/.config/scripts/monitor-power.sh --off' \
     resume '$HOME/.config/scripts/monitor-power.sh --on && $HOME/.config/scripts/monitor-scale.sh' \
     before-sleep 'swaylock -f -c 000000' 
 
