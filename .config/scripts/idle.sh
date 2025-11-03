d5dc319a9a5efd2110ea07c4d2018e3b4df49368
areif-dev <aj@ajreifsnyder.com>
2025-11-02 23:31:46 -0500

    scripts: increase time to screen lock to 10 minutes

--- .config/scripts/idle.sh
+++ .config/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 # Lock the screen after 2 minutes of inactivity. Turn the monitor off and suspend power after 20 minutes
-swayidle -w timeout 120 'swaylock -f -c 000000 && $HOME/.config/scripts/monitor-power.sh --off' \
+swayidle -w timeout 600 'swaylock -f -c 000000 && $HOME/.config/scripts/monitor-power.sh --off' \
     resume '$HOME/.config/scripts/monitor-power.sh --on && $HOME/.config/scripts/monitor-scale.sh' \
     before-sleep 'swaylock -f -c 000000' 
 
